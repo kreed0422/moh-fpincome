2ed1858ae185fd821e93d96b9b16d867ea287539
kristin reed <kristin.reed@maximusbc.ca>
2020-05-15 01:26:00 -0400

    chore: update package version cmd

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,10 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'd5da7f9';
-export const buildTime = '5/13/2020 at 2:59:42 PM';
+export const gitCommit = '4f39eb3';
+export const buildTime = '5/14/2020 at 9:58:54 PM';
 /** App version retrieved from package.json. */
-export const projectVersion = '0.1.0';
+export const projectVersion = '0.1.3';
 /** Human readable message  */
-export const message = 'Canonical Version: d5da7f9 - 5/13/2020 at 2:59:42 PM (v0.1.0) \nhttps://github.com/bcgov/moh-bcp/commits/d5da7f9';
+export const message =
+  'Canonical Version: 4f39eb3 - 5/14/2020 at 9:58:54 PM (v0.1.3) \nhttps://github.com/bcgov/moh-bcp/commits/4f39eb3';
 /** If true, other values should be present. */
 export const success = true;

5efc2864d07eac2b6cd1442aad29c47d6692a4b2
kristin reed <kristin.reed@maximusbc.ca>
2020-05-20 13:31:01 -0400

    chore: add todo in collectiveNoticeDirective

--- src/app/income-review/component/collection-notice/collection-notice.directive.ts
+++ src/app/income-review/component/collection-notice/collection-notice.directive.ts
@@ -1,5 +1,6 @@
 import { Directive } from '@angular/core';
 
+/** TODO: Use FPCare Public Web as template for directive to keep focus within modal */
 @Directive({
   selector: '[fpirCollectionNotice]',
 })

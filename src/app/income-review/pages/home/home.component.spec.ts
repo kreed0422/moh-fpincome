bc6f3c0e8d6999c5b915d07c6174a52f94cd7414
kristin reed <kristin.reed@maximusbc.ca>
2020-06-01 18:58:28 -0400

    chore: remove unused variable

--- src/app/income-review/pages/home/home.component.spec.ts
+++ src/app/income-review/pages/home/home.component.spec.ts
@@ -1,9 +1,4 @@
-import {
-  async,
-  ComponentFixture,
-  TestBed,
-  fakeAsync,
-} from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';

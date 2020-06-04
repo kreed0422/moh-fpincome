acad59f720ee814fb218f2019b364174522ab767
kristin reed <kristin.reed@maximusbc.ca>
2020-06-04 16:58:01 -0400

    feat(fpcir-70): update content on supporting documents page

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,10 +1,10 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '6d9a4be';
-export const buildTime = '6/3/2020 at 9:05:35 PM';
+export const gitCommit = 'b6ea65f';
+export const buildTime = '6/4/2020 at 1:48:38 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.2.1';
 /** Human readable message  */
 export const message =
-  'Canonical Version: 6d9a4be - 6/3/2020 at 9:05:35 PM (v0.2.1) \nhttps://github.com/bcgov/moh-bcp/commits/6d9a4be';
+  'Canonical Version: b6ea65f - 6/4/2020 at 1:48:38 PM (v0.2.1) \nhttps://github.com/bcgov/moh-bcp/commits/b6ea65f';
 /** If true, other values should be present. */
 export const success = true;

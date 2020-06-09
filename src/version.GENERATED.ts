db24156be9aab464978d6a00e607945609900117
kristin reed <kristin.reed@maximusbc.ca>
2020-06-09 16:26:24 -0400

    chore(lint): fix line length

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,10 +1,11 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '1620c00';
-export const buildTime = '6/9/2020 at 12:54:27 PM';
+export const gitCommit = '250d0e5';
+export const buildTime = '6/9/2020 at 1:20:09 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.3.0';
 /** Human readable message  */
 export const message =
-  'Canonical Version: 1620c00 - 6/9/2020 at 12:54:27 PM (v0.3.0) \nhttps://github.com/bcgov/moh-fpincome/commits/1620c00';
+  'Canonical Version: 250d0e5 - 6/9/2020 at 1:20:09 PM (v0.3.0)' +
+  '\nhttps://github.com/bcgov/moh-fpincome/commits/250d0e5';
 /** If true, other values should be present. */
 export const success = true;

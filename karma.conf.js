90e828bbb1e14299d8528f1b8e38ac89908d12a3
kristin reed <kristin.reed@maximusbc.ca>
2020-11-20 00:12:28 -0500

    test(home): fix test, add firefox for headless tests

--- karma.conf.js
+++ karma.conf.js
@@ -8,31 +8,31 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-spec-reporter')
+      require('karma-spec-reporter'),
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/moh-fpincome'),
       reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
     },
     // reporters: ['progress', 'kjhtml'],
     reporters: ['spec'],
     specReporter: {
-       maxLogLines: 5, // logged lines per test
+      maxLogLines: 5, // logged lines per test
     },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
   });
 };
-

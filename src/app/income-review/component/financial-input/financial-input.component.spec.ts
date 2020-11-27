5b747a4cc87006556bc7b445ba404018db6b9b93
kristin reed <kristin.reed@maximusbc.ca>
2020-11-26 20:05:45 -0500

    test: add missing component to tests

--- src/app/income-review/component/financial-input/financial-input.component.spec.ts
+++ src/app/income-review/component/financial-input/financial-input.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TextMaskModule } from 'angular2-text-mask';
 
 import { FinancialInputComponent } from './financial-input.component';
 
@@ -9,6 +10,7 @@ describe('FinancialInputComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [FinancialInputComponent],
+      imports: [TextMaskModule],
     }).compileComponents();
   }));
 
